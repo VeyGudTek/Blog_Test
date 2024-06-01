a433c8f108167656a2afdb254279d80fb42cc8ce
Tim Lin <lintim03@gmail.com>
2024-05-31 21:20:50 -0500

    Added Dynamic values to App.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title = 'Tutorial Blog'
+  const likes = 50
+  const link = "https://www.youtube.com/"
+
   return (
     <div className="App">
       <div className="content">
-        <h1>Test Component</h1>
+        <h1>{title}</h1>
+        <p>Liked {likes} times</p>
+
+        <p>{ Math.random(10) * 10 }</p>
+
+        <a href={link}>youtube link</a>
       </div>
     </div>
   );
