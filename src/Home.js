a6eea05464322b9ca60f7a4868bda321176b2d1c
Tim Lin <lintim03@gmail.com>
2024-05-31 22:23:34 -0500

    Added Click Events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,20 @@
 const Home = () => {
+    const handleClick = (e) => {
+        console.log('Wow')
+        console.log(e)
+    }
+
+    const handleCLickAgain = (name, e) => {
+        console.log(name)
+        console.log(e.target)
+    }
+
     return ( 
         <div className="home">
             <h2>Home Page</h2>
+
+            <button onClick={handleClick}>Click Event</button>
+            <button onClick={(e)=>{handleCLickAgain('Elma', e)}}>Click Event with Arguments</button>
         </div>
     )
 }
