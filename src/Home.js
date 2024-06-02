b0aa17b8ace2e281b3115f43a4910b6f2ecdd81f
Tim Lin <lintim03@gmail.com>
2024-06-02 14:15:32 -0500

    Added state hooks to Home

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,12 @@
+import { useState } from 'react'
+
 const Home = () => {
+    const [name, setName] = useState('Elma')
+    const [counter, setCounter] = useState(1)
+
     const handleClick = (e) => {
+        setName('Amy')
+        setCounter(counter + 1)
         console.log('Wow')
         console.log(e)
     }
@@ -13,6 +20,7 @@ const Home = () => {
         <div className="home">
             <h2>Home Page</h2>
 
+            <p>{name} - {counter}</p>
             <button onClick={handleClick}>Click Event</button>
             <button onClick={(e)=>{handleCLickAgain('Elma', e)}}>Click Event with Arguments</button>
         </div>
