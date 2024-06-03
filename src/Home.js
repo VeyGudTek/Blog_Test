2c58529e10e850abfe535e7f7e8587f4f64841d9
Tim Lin <lintim03@gmail.com>
2024-06-02 20:27:27 -0500

    Passed Function to BlogList as Prop

--- src/Home.js
+++ src/Home.js
@@ -5,12 +5,20 @@ const Home = () => {
     const [blogs, setBlogs] = useState([
         {title: "Hungry Nicole", body: "Sheep Sheep Sheep Sheep", author: "Elma", id: 1},
         {title: "Siren", body: "La La La La La La", author: "N-Buna", id: 2},
-        {title: "ThoughtCrime", body: "I want to eat your words up completely", author: "Yorushika", id: 3}
+        {title: "ThoughtCrime", body: "I want to eat your words up completely", author: "Yorushika", id: 3},
+        {title: "In May", body: "From and Emerald Green Window", author: "Elma", id: 4},
+        {title: "August", body: "Moonlight, a Certain Person", author: "Elma", id: 5}
     ])
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter((blog) => blog.id !== id)
+        setBlogs(newBlogs)
+    }
 
     return ( 
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs"/>
+            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
+            <BlogList blogs={blogs.filter((blog) => blog.author === "Elma")} title="Elma's Blogs" handleDelete={handleDelete}/>
         </div>
     )
 }
