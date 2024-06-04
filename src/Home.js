88833518f1f7bdd74dd02ebaa63065a2fe431577
Tim Lin <lintim03@gmail.com>
2024-06-03 19:40:03 -0500

    useEffect Hook to fetch data, removed clientside delete

--- src/Home.js
+++ src/Home.js
@@ -2,15 +2,7 @@ import { useState, useEffect } from 'react'
 import BlogList from './BlogList'
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        {title: "Hungry Nicole", body: "Sheep Sheep Sheep Sheep", author: "Elma", id: 1},
-        {title: "Siren", body: "La La La La La La", author: "N-Buna", id: 2},
-        {title: "ThoughtCrime", body: "I want to eat your words up completely", author: "Yorushika", id: 3},
-        {title: "In May", body: "From and Emerald Green Window", author: "Elma", id: 4},
-        {title: "August", body: "Moonlight, a Certain Person", author: "Elma", id: 5}
-    ])
-
-    const [name, setName] = useState('Elma')
+    const [blogs, setBlogs] = useState(null)
 
     const handleDelete = (id) => {
         const newBlogs = blogs.filter((blog) => blog.id !== id)
@@ -18,15 +10,18 @@ const Home = () => {
     }
 
     useEffect(() => {
-        console.log('use effect')
-    }, [name])
+        fetch("http://localhost:8000/blogs")
+        .then((res) => {
+            return res.json()
+        })
+        .then((data) => {
+            setBlogs(data)
+        })
+    }, [])
 
     return ( 
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
-            <BlogList blogs={blogs.filter((blog) => blog.author === "Elma")} title="Elma's Blogs" handleDelete={handleDelete}/>
-            <button onClick={() => setName('Amy')}>Set Name</button>
-            <p>{name}</p>
+            {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
         </div>
     )
 }
